8c16fedeeccc845c044d343515eb2624619f6bfe
VEGETv07 <mouad07m20@gmail.com>
2023-12-10 16:48:10 +0100

    new update

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template, request
 from pymongo import MongoClient
 import requests
 import matplotlib.pyplot as plt
+from datetime import datetime 
 
 
 app = Flask(__name__)
 # api_key
-# https://api.weatherapi.com/v1/forecast.json?q=agadir&key=db36df6e32e943a8b65204853233011
 api_key = 'db36df6e32e943a8b65204853233011'
 # connexion a MongoDB:
 client = MongoClient('mongodb://localhost:27017/')
@@ -16,16 +16,23 @@ collection = db['weather_collection']
 @app.route("/")
 def index():
     return render_template('index.html', title= "Weather", style='forecast')
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About', style='about')
 
-@app.route('/search', methods=['POST'])
-def search():
-    if request.method == "POST":
-        city = request.form['city']
+# ------------========--------------------------
+def fetch_data_from_api(city):
         url = f'https://api.weatherapi.com/v1/forecast.json?q={city}&key={api_key}&days=7'
-
         response = requests.get(url)
-        data = response.json()
-        # clear existing city
+        return response.json()
+
+def error_city_not_exist(data):
+    if 'error' in data:
+        error_message = f'Error: {data["error"]["message"]}'
+        return True, render_template('index.html', error_message=error_message, style="forecast")
+    return False, None
+    
+def insert_data_into_mongodb(city, data):
         collection.delete_many({'city': city})
         data_to_insert = {
                 'city' : city,
@@ -34,39 +41,62 @@ def search():
         # insert the data into mongoDB
         collection.insert_one(data_to_insert)
 
-        ddd = request.form.get('city')
-        weather_data = list(collection.find({'city': ddd},{"_id":0}))
+def generate_plot(city):
+    weather_data = list(collection.find({'city': city},{"_id":0}))
+    humidity =[day['day']['avghumidity'] for day in weather_data[0]['api_response']['forecast']['forecastday']]
+    max_temperature_week = [week['day']['maxtemp_c'] for week in weather_data[0]['api_response']['forecast']['forecastday']]
+    min_temperature_week = [week['day']['mintemp_c'] for week in weather_data[0]['api_response']['forecast']['forecastday']]
+    temperature_hour = [hour['temp_c'] for hour in weather_data[0]['api_response']['forecast']['forecastday'][0]['hour']]
+    timestamp = [hour['time_epoch'] for hour in weather_data[0]['api_response']['forecast']['forecastday'][0]['hour']]
+    time = [datetime.utcfromtimestamp(ts).strftime('%H:%M') for ts in timestamp]
+    day = [day['date'] for day in weather_data[0]['api_response']['forecast']['forecastday']]
 
-        humidity =[day['day']['avghumidity'] for day in weather_data[0]['api_response']['forecast']['forecastday']]
-        max_temperature_week = [week['day']['maxtemp_c'] for week in weather_data[0]['api_response']['forecast']['forecastday']]
-        min_temperature_week = [week['day']['mintemp_c'] for week in weather_data[0]['api_response']['forecast']['forecastday']]
-        temperature_hour = [hour['temp_c'] for day in weather_data[0]['api_response']['forecast']['forecastday'] if 'hour' in day for hour in day['hour']]
-        time = [hour['time'] for day in weather_data[0]['api_response']['forecast']['forecastday'] if 'hour' in day for hour in day['hour']]
-        day = [day['date'] for day in weather_data[0]['api_response']['forecast']['forecastday']]
 
-        fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(10, 12))
+    # print(len(temperature_hour))
+    # print(len(timestamp))
+    fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(14, 16))
+    fig.set_facecolor('#e7e7e7')
 
-        # Plotting humidity
-        ax1.plot(day, humidity, color='r', label='Humidity')
-        ax1.set_xlabel('Day')
-        ax1.set_ylabel('Humidity')
-        ax1.legend()
+    # Plotting humidity
+    ax1.plot(day, humidity, color='r', label='Humidity')
+    ax1.set_xlabel('Day')
+    ax1.set_ylabel('Humidity')
+    ax1.legend()
 
-        # Plotting temperature
-        ax2.plot(time, temperature_hour, color='b', label='Temperature')
-        ax2.set_xlabel('Day')
-        ax2.set_ylabel('Temperature')
-        ax2.legend()
+    # Plotting temperature
+    ax2.plot(time, temperature_hour, color='b', label='Temperature')
+    ax2.set_xlabel('Time')
+    ax2.set_ylabel('Temperature')
+    ax2.set_title('Forecast for today')
+    ax2.legend()
 
-        # Plotting min and max temperature over time
-        ax3.plot(day, min_temperature_week, color='g', label='Min Temperature')
-        ax3.plot(day, max_temperature_week, color='y', label='Max Temperature')
-        ax3.set_xlabel('Time')
-        ax3.set_ylabel('Temperature')
-        ax3.legend()
+    # Plotting min and max temperature over time
+    ax3.plot(day, max_temperature_week, color='y', label='Temperature Max')
+    ax3.plot(day, min_temperature_week, color='g', label='Temperature Min')
+    ax3.set_xlabel('Day')
+    ax3.set_ylabel('Temperature')
+    ax3.legend()
 
-        # Save the plot to an image file
-        plt.savefig('static/forecast_plot.png')
+    # Save the plot to an image file
+    plt.savefig('static/forecast_plot.png')
+    return 'static/forecast_plot.png'
+     
+@app.route('/', methods=['POST'])
+def search():
+    if request.method == "POST":
+        city = request.form['city'].lower()
+        data = fetch_data_from_api(city)
+        # clear existing city
+        collection.delete_many({'city': city})
+        has_error, error_template = error_city_not_exist(data)
+        if has_error:
+            return error_template
+        insert_data_into_mongodb(city, data)
+
+        weather_data = list(collection.find({'city': city},{"_id":0}))
+
+        generate_plot(city)
+
 
     return render_template('index.html', title="Search",  data=weather_data, style='forecast', image='static/forecast_plot.png')
 
